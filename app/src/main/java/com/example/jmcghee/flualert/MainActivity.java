0889a3451aef745fe7941f269af2a1ce0ac031ab
Mcghee-Jake <jmcghee@csumb.edu>
2018-08-21 13:54:06 -0700

    added loading indicator and made data persist after screen rotation

--- app/src/main/java/com/example/jmcghee/flualert/MainActivity.java
+++ app/src/main/java/com/example/jmcghee/flualert/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.jmcghee.flualert;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.jmcghee.flualert.utils.NetworkUtils;
@@ -13,7 +15,10 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private static final String NUM_DAYS = Integer.toString(7);
+    private static final String SEARCH_RESULTS_RAW_JSON = "results"; // Used to save and restore the results of the API call
+
     private TextView tvTest;
+    private ProgressBar pbLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +26,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvTest = findViewById(R.id.tv_test);
+        pbLoadingIndicator = findViewById(R.id.pb_loading_indicator);
 
-        makeQuery();
+        if (savedInstanceState != null) {
+            String rawJsonSearchResults = savedInstanceState.getString(SEARCH_RESULTS_RAW_JSON);
+            tvTest.setText(rawJsonSearchResults);
+        } else {
+            makeQuery();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        String rawJsonSearchResults = tvTest.getText().toString();
+        outState.putString(SEARCH_RESULTS_RAW_JSON, rawJsonSearchResults);
     }
 
     private void makeQuery() {
@@ -54,7 +73,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pbLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
         protected void onPostExecute(String s) {
+            pbLoadingIndicator.setVisibility(View.INVISIBLE);
+
             if (s != null && !s.equals("")) {
                tvTest.setText(s);
             }
